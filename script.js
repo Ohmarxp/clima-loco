33d43d292eca05ad0dd1e54e13f4dfd45f205e04
Marxho <marxhop@hotmail.com>
2023-02-08 16:26:16 -0500

    Declared variables on js file

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('What\'s up!')
+
+//Declaring variables
+let boton = document.getElementById('buscar-boton');
+let climaActualArticulos = document.getElementById('clima-actual-articulos');
+let fecha = document.getElementById('fecha');
+let tiempo = document.getElementById('tiempo');
+let pais = document.getElementById('pais');
+let viento = document.getElementById('viento');
+let dia = document.querySelector('.dia')
+let ciudad = document.querySelector('.ciudad');
+let temperatura = document.querySelector('temperatura');
+let humedad = document.getElementById('humedad');
